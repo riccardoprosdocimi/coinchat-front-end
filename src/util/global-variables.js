dcbd6134671a537a08d1298a76190233dcf93b8b
Riccardo Prosdocimi <95254927+riccardoprosdocimi@users.noreply.github.com>
2024-06-11 00:16:57 -0400

    Rename global variable

--- src/util/global-variables.js
+++ src/util/global-variables.js
@@ -1,11 +1,11 @@
-const Domain_address = process.env.REACT_APP_BASE_URL;
+const DOMAIN_ADDRESS = process.env.REACT_APP_BASE_URL;
 
-export const COINGECKO_API_BASE_URL = 'https://api.coingecko.com/api/v3/coins'
-export const coinGeckoSearch_API = "https://api.coingecko.com/api/v3/search?query="
+export const COINGECKO_API_BASE_URL = 'https://api.coingecko.com/api/v3/coins';
+export const coinGeckoSearch_API = "https://api.coingecko.com/api/v3/search?query=";
 
-export const Search_API = `${Domain_address}/search/coinSearch?query=`;
-export const CoinData_API = `${Domain_address}/detail/getCoinData?coinID=`;
-export const CoinMarketChartAPI = `${Domain_address}/detail/getCoinMarketChart?coinID=`;
-export const Comment_API = `${Domain_address}/api/comment`;
-export const User_API = `${Domain_address}/api/users`
-export const Blog_API = `${Domain_address}/api/blog`
+export const Search_API = `${DOMAIN_ADDRESS}/search/coinSearch?query=`;
+export const CoinData_API = `${DOMAIN_ADDRESS}/detail/getCoinData?coinID=`;
+export const CoinMarketChartAPI = `${DOMAIN_ADDRESS}/detail/getCoinMarketChart?coinID=`;
+export const Comment_API = `${DOMAIN_ADDRESS}/api/comment`;
+export const User_API = `${DOMAIN_ADDRESS}/api/users`;
+export const Blog_API = `${DOMAIN_ADDRESS}/api/blog`;
